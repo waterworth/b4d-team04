40e8cbdaebbb6249d8a01f28b88624c17c612373
Tony <unclestrong@Tonys-MacBook-Air.local>
2020-12-03 18:34:45 -0800

    added first route and removed extra comments from data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const inventoryRoutes = require('./routes/inventory')
 
 const port = 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use('/inventory', inventoryRoutes);
 
 app.use('/static', express.static('public'));
 
