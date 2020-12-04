323373b93c57bccb7a812678085551247234ed74
Tony <unclestrong@Tonys-MacBook-Air.local>
2020-12-03 20:53:54 -0800

    back-end completed

--- server/index.js
+++ server/index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const inventoryRoutes = require('./routes/inventory')
+const inventoryRoutes = require('./routes/inventory');
+const storeRoutes = require('./routes/stores');
 
 const port = 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use('/inventory', inventoryRoutes);
+app.use('/stores', storeRoutes);
 
-app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.json({
+    routes: [
+      {
+        method: 'get',
+        endpoint: '/inventory',
+      },
+      {
+        method: 'get',
+        endpoint: '/inventory/:id',
+      },
+      {
+        method: 'get',
+        endpoint: '/stores',
+      },
+      {
+        method: 'get',
+        endpoint: '/stores/:id',
+      },
+    ],
+  });
+});
 
 app.listen(port, () => {
   console.log(`Express is listening on port ${port}`);
--- server/routes/stores.js
+++ server/routes/stores.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const inventoryList = path.join(__dirname, '../data/inventory.json');
+const storeList = path.join(__dirname, '../data/stores.json');
 const functions = require('../utilities/functions');
 
 router.get('/', (req, res) => {
-  res.json(functions.getDataByList(inventoryList));
+  res.json(functions.getDataByList(storeList));
 });
 
 router.get('/:id', (req, res) => {
-  res.json(functions.getDataByID(req.params.id, inventoryList));
+  res.json(functions.getDataByID(req.params.id, storeList));
 });
 
 module.exports = router;
